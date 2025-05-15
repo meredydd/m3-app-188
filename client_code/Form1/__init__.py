06e4149f64baf0f23c91db3bfaab3b1374cbbd13
meredydd@anvil.works <meredydd@anvil.works>
2025-05-15 21:37:42 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +12,9 @@ class Form1(Form1Template):
         self.init_components(**properties)
 
         # Any code you write here will run before the form opens.
+
+    def outlined_button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.message_lbl.text = f"Hello, {self.name_box.text}!"
+        r = anvil.server.call('say_hello', self.name_box.text)
+        alert(f"Function returned {r}")
